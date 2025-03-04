a7d76302fa59d4bf5aa7048a504a1e135137aa59
Evan Eissler <evaneissler@icloud.com>
2025-03-04 16:57:25 -0600

    main setup

--- learn-vue-components/src/App.vue
+++ learn-vue-components/src/App.vue
@@ -1,21 +1,13 @@
 <template>
-  <ButtonCounter></ButtonCounter>
-  <ButtonCounter></ButtonCounter>
+  <div>
 
-  <button-counter></button-counter>
-
-  <hr />
-
-  <div style="background-color: orange; padding: 5px">
-    This is the root component.
-    <ComponentA></ComponentA>
-    <ComponentA></ComponentA>
   </div>
 </template>
 
 <script setup>
-import ButtonCounter from './ButtonCounter.vue'
-import ComponentA from '@/ComponentA.vue'
+
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+
+</style>
